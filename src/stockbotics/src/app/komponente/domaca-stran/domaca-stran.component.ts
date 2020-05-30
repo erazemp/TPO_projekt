2382ce392f392e1022f5618399a36c8522b1fe84
ds9825 <ds9825@student.uni-lj.si>
2020-05-30 17:10:10 +0200

    dodan podrobni prikaz delnice z grafom na domači strani #12, dodan dependency na chart.js

--- src/stockbotics/src/app/komponente/domaca-stran/domaca-stran.component.ts
+++ src/stockbotics/src/app/komponente/domaca-stran/domaca-stran.component.ts
@@ -4,6 +4,8 @@ import {DatePipe} from "@angular/common";
 import {StreznikPodatkiService} from "../../storitve/streznik-podatki.service";
 import {Podjetje} from "../../razredi/podjetje";
 import {ZgodovinskiPodatek} from "../../razredi/zgodovinskiPodatek";
+import {Data} from "@angular/router";
+import {Chart} from "chart.js";
 
 @Component({
   selector: 'app-domaca-stran',
@@ -13,8 +15,15 @@ import {ZgodovinskiPodatek} from "../../razredi/zgodovinskiPodatek";
 export class DomacaStranComponent implements OnInit {
 
   private seznamPodjetij: Array<Podjetje>;
-  private trenutnePodjetjeDelnice: Array<ZgodovinskiPodatek>;
+  private trenutnoPodjetjeDelnice: Array<ZgodovinskiPodatek>;
   private trenutnoPodjetjeIme: string;
+  private trenutnoPodjetjeSimbol: string;
+
+  private imaTrenutnoPodjetjePodatkeODelnicah = false;
+  private data: Data[];
+  private adjustedArray;
+  private datumArray;
+  private linechart = null;
 
   constructor(private title: Title,
               private streznikService: StreznikPodatkiService,
@@ -29,21 +38,91 @@ export class DomacaStranComponent implements OnInit {
       });
   }
 
-  private imaTrenutnoPodjetjePodatkeODelnicah() {
-    if (this.trenutnePodjetjeDelnice != null && Array.isArray(this.trenutnePodjetjeDelnice) && this.trenutnePodjetjeDelnice.length > 0)
-      return true;
-    else
-      return false;
-  }
-
   private pridobiZgodovinskePodatkePodjetja(podjetje: Podjetje): void {
     this.streznikService.prikaziZgodovinskePodatke(podjetje.simbol)
       .then(odgovor => {
-        this.trenutnePodjetjeDelnice = odgovor;
+        this.trenutnoPodjetjeDelnice = odgovor;
         this.trenutnoPodjetjeIme = podjetje.ime;
+        this.trenutnoPodjetjeSimbol = podjetje.simbol;
         console.log("trenutna delnica:");
-        console.log(this.trenutnePodjetjeDelnice);
+        console.log(this.trenutnoPodjetjeDelnice);
+        this.nastaviGraf();
       });
+  }
+
+  public nastaviGraf() {
+    if (this.trenutnoPodjetjeDelnice != null && Array.isArray(this.trenutnoPodjetjeDelnice) && this.trenutnoPodjetjeDelnice.length > 0) {
+
+      this.adjustedArray = [];
+      this.datumArray = [];
+
+      this.trenutnoPodjetjeDelnice.forEach(a => {
+        this.adjustedArray.push(a.adjusted);
+        this.datumArray.push(a.datum);
+      });
+
+      console.log(this.adjustedArray);
+      console.log(this.datumArray);
+
+      this.linechart = new Chart('canvasDiv', {
+        type: 'line',
+        data: {
+          labels: this.datumArray,
+          datasets: [
+            {
+              data: this.adjustedArray,
+              borderColor: '#3cb371',
+              backgroundColor: "#0000FF",
+            }
+          ]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          scales: {
+            xAxes: [{
+              display: true
+            }],
+            yAxes: [{
+              display: true
+            }],
+          }
+        }
+      });
+
+    }
+    else {
+      this.adjustedArray = [];
+      this.datumArray = [];
+
+      this.linechart = new Chart('canvasDiv', {
+        type: 'line',
+        data: {
+          labels: this.datumArray,
+          datasets: [
+            {
+              data: this.adjustedArray,
+              borderColor: '#3cb371',
+              backgroundColor: "#0000FF",
+            }
+          ]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          scales: {
+            xAxes: [{
+              display: true
+            }],
+            yAxes: [{
+              display: true
+            }],
+          }
+        }
+      });
+    }
   }
 
   ngOnInit() {
